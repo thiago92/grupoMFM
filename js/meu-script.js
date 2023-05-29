12cba2f85eabc1dcd11a923445afde818fbb1ac7
Thiago Da Silva Souza <thiago.silva@grupomfm.com.br>
2023-05-29 14:04:14 -0300

    wowVisibility

--- js/meu-script.js
+++ js/meu-script.js
@@ -1,26 +1,2 @@
 
-  new WOW().init();
-  // Após adicionar dinamicamente o elemento do cabeçalho
-  var headerElement = document.querySelector('header');
-  // ...
 
-  // Atualizar o WOW.js para animar o novo elemento do cabeçalho
-  wow.refresh();
-  // Após adicionar dinamicamente o conteúdo na seção "Sobre o Grupo"
-  var sobreGrupoElement = document.querySelector('.secaoSobreOGrupo');
-  // ...
-
-  // Atualizar o WOW.js para animar o novo conteúdo
-  wow.refresh();
-  // Após adicionar dinamicamente as imagens das empresas
-  var empresasElement = document.querySelector('.empresas');
-  // ...
-
-  // Atualizar o WOW.js para animar as novas imagens
-  wow.refresh();
-  // Após adicionar dinamicamente o conteúdo no rodapé
-  var footerElement = document.querySelector('footer');
-  // ...
-
-  // Atualizar o WOW.js para animar o novo conteúdo do rodapé
-  wow.refresh();
